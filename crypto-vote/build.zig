f48ee1ebf9212dcaaf7d3bcda8b5199e9cb9184d
Oskar Hahn <mail@oshahn.de>
2025-06-07 23:06:43 +0200

    Build wasm file to wrapper

--- crypto-vote/build.zig
+++ crypto-vote/build.zig
@@ -13,5 +13,13 @@ pub fn build(b: *std.Build) void {
     wasm.rdynamic = true;
     wasm.entry = .disabled;
 
-    b.installArtifact(wasm);
+    const wf = b.addUpdateSourceFiles();
+    wf.addCopyFileToSource(wasm.getEmittedBin(), "wrapper/crypto_vote.wasm");
+
+    const update_wasm_step = b.step("wasm", "Update crypto_vote.wasm");
+    update_wasm_step.dependOn(&wf.step);
+
+    b.default_step = update_wasm_step;
+
+    //b.installArtifact(wasm);
 }
